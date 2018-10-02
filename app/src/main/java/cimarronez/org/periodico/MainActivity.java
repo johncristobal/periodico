d7f049a7c70a6b02189b4eeaf70096ee448a8bc1
John_Vera <johncristobal@users.noreply.github.com>
2018-10-01 23:05:31 -0500

    iamge up

--- app/src/main/java/cimarronez/org/periodico/MainActivity.java
+++ app/src/main/java/cimarronez/org/periodico/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,13 +24,18 @@ public class MainActivity extends AppCompatActivity {
     private static final long SPLASH_SCREEN_DELAY=4000;
     public static FirebaseAuth mAuth;
     public static FirebaseUser user;
-
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TimerTask task=new TimerTask() {
+
+        mImageView=(ImageView)findViewById(R.id.imageView5);
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+        /*TimerTask task=new TimerTask() {
             @Override
             public void run() {
 
@@ -52,7 +60,24 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         Timer timer= new Timer();
-        timer.schedule(task,SPLASH_SCREEN_DELAY);
+        timer.schedule(task,SPLASH_SCREEN_DELAY);*/
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        mScaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 
     public void updateUI(FirebaseUser u)
